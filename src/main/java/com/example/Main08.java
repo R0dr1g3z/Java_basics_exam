1d899991c098981e4b3cfba87ea787bc38e598fe
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-13 18:47:53 +0100

    remove useless imports

--- src/main/java/com/example/Main08.java
+++ src/main/java/com/example/Main08.java
@@ -2,10 +2,6 @@ package com.example;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main08 {
